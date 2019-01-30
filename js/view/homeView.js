e8a11ab48806bdde6dbb78d1702e555e76c6e8af
Kata-M <martikai@kth.se>
2019-01-30 23:51:20 +0100

    remove unnescessary files

--- js/view/homeView.js
+++
@@ -1,6 +0,0 @@
-function getSelectDishView() {
-  var hide = document.getElementById("homeView");
-  var show = document.getElementById("dishView");
-    hide.style.display = "none";
-    show.style.display = "block";
-}
--- js/view/sidebarViewController.js
+++
@@ -1,5 +0,0 @@
- // sidebarViewController.js
-var SidebarViewController = function(view,model, app) { view.confirmButton.click(function(){
-         app.showConfimScreen();
-     })
-}
